50610d1e332abdec1fbe359b59add3352583b1ea
Phạm Quang Trung <phamcham@users.noreply.github.com>
2019-10-12 11:00:56 +0700

    sua 9 10

--- Day 2/Bai10/src/bai10/Bai10.java
+++ Day 2/Bai10/src/bai10/Bai10.java
@@ -3,17 +3,30 @@ import java.util.*;
 public class Bai10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
         double del = b * b - 4 * a * c;
-        if (del < 0){
+        if (a == 0){
+            if (b == 0){
+                if (c == 0) System.out.println("Vo so nghiem");
+                else System.out.println("Khong co nghiem");
+            }
+            else{
+                System.out.println("Nghiem: " + -c/b);
+            }
+        }
+        else if (del < 0){
             System.out.println("Phuong trinh vo nghiem");
         }
         else if (del > 0){
             double x1 = (-b - Math.sqrt(del)) / (2 * a);
             double x2 = (-b + Math.sqrt(del)) / (2 * a);
             System.out.println("Nghiem: x1 = " + x1 + "\nx2 = " + x2);
+        }
+        else{
+            double x = -b / (2 * a);
+            System.out.println("Nghiem kep: x = " + x);
         }
     }
 }
--- Day 2/Bai9/src/bai9/Bai9.java
+++ Day 2/Bai9/src/bai9/Bai9.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Bai9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        double a = sc.nextInt();
+        double b = sc.nextInt();
         if (a == 0){
             if (b == 0) System.out.println("Vo so nghiem");
             else System.out.println("Khong co nghiem");
