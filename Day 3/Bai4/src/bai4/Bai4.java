f928259362a9d7d7d15eed0bb13864f4c5d6f897
Phạm Quang Trung <phamcham@users.noreply.github.com>
2019-10-19 00:09:19 +0700

    update

--- Day 3/Bai4/src/bai4/Bai4.java
+++ Day 3/Bai4/src/bai4/Bai4.java
@@ -1,12 +1,19 @@
 package bai4;
-import java.math.*;
 import java.util.*;
 public class Bai4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BigInteger a = in.nextBigInteger();
-        BigInteger b = in.nextBigInteger();
-        System.out.println(a.add(b));
+        String s = in.next();
+        String helu = "hello";
+        
+        int p = 0;
+        for (char c : s.toCharArray()){
+            if (p < helu.length() && c == helu.charAt(p)){
+                p++;
+            }
+        }
+        if (p < 5) System.out.println("No");
+        else System.out.println("Yes");
     }
     
 }
